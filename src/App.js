22e4962842c8ad9c80ca4093060b8a8c63b90acb
Breyanna Vass <breyannavass@gmail.com>
2019-11-26 18:25:03 -0500

    making a users details function

--- src/App.js
+++ src/App.js
@@ -11,4 +11,18 @@ const App = () => (
   </div>
 );
 
+const ProfileDetails = () => {
+  const user = resource.user.read();
+  return (
+    <div className="card card-body my-3">
+      <h1 className="large text-primary">{user.name}</h1>
+      <ul>
+        <li>Username: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>City: {user.address.city}</li>
+      </ul>
+    </div>
+  );
+};
+
 export default App;
