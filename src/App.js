bbebb00e17177aadd4eaf13a3552e1a3b865f07a
Breyanna Vass <breyannavass@gmail.com>
2019-11-26 18:20:56 -0500

    import the fetch data on the app file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
-
+import React, { Suspense } from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+import { fetchData } from "./Api";
+
+const resource = fetchData();
+
+const App = () => (
+  <div className="container my-5">
+    <h1>Hello World</h1>
+  </div>
+);
 
 export default App;
