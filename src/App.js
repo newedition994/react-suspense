9ca1480fea4a421d5f9e2616f15008e44860a96d
Breyanna Vass <breyannavass@gmail.com>
2019-11-26 18:28:22 -0500

    making a profile post function

--- src/App.js
+++ src/App.js
@@ -17,11 +17,27 @@ const ProfileDetails = () => {
     <div className="card card-body my-3">
       <h1 className="large text-primary">{user.name}</h1>
       <ul>
-        <li>Username: {user.name}</li>
+        <li>Username: {user.username}</li>
         <li>Email: {user.email}</li>
         <li>City: {user.address.city}</li>
       </ul>
     </div>
+  );
+};
+
+const ProfilePosts = () => {
+  const posts = resource.posts.read();
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">
+        <strong>Latest Posts</strong>
+      </li>
+      {posts.map(post => (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      ))}
+    </ul>
   );
 };
 
